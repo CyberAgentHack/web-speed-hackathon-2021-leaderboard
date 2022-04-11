676d66024d022db46e8681ae1fc5d9a4d65f69bc
chimame <rito.tamata@gmail.com>
2022-04-11 15:40:52 +0900

    Add create Measurement function (#40)

--- scripts/measure/src/database.ts
+++ scripts/measure/src/database.ts
@@ -2,11 +2,29 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseClient = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
 
+export const fetchTeamInfo = async (queueId: string) => {
+  const { data, error } = await supabaseClient.from<{Team: {id: string, pageUrl: string}}>('Queue').select('Team(id, pageUrl)').match({id: queueId});
+  if (error?.message) {
+    console.error(error.message);
+    throw new Error(error.message);
+  }
+  return data[0]?.Team;
+}
+
 export const updateQueueStatus = async (id: string, status: 'RUNNING' | 'FAILED' | 'DONE') => {
   const { data, error } = await supabaseClient.from('Queue').update({status}).match({id});
   if (error?.message) {
     console.error(error.message);
     throw new Error(error.message);
   }
-  return { data };
+  return data;
+}
+
+export const createMeasurement = async (teamId: string, score: number) => {
+  const { data, error } = await supabaseClient.from('Measurement').insert({teamId, score});
+  if (error?.message) {
+    console.error(error.message);
+    throw new Error(error.message);
+  }
+  return data;
 }
--- scripts/measure/src/index.ts
+++ scripts/measure/src/index.ts
@@ -2,7 +2,7 @@ import fastify from 'fastify';
 import { main as mainVrt } from '@web-speed-hackathon/vrt/src';
 import { main as mainScoring } from '@web-speed-hackathon/scoring/src';
 import { execSync } from 'child_process';
-import { updateQueueStatus } from './database';
+import { updateQueueStatus, createMeasurement, fetchTeamInfo } from './database';
 
 const server = fastify();
 
@@ -15,18 +15,22 @@ server.post<{
 }>('/execute/:queueId', async (request, reply) => {
   try {
     await updateQueueStatus(request.params.queueId, 'RUNNING');
-    // TODO: target url from request.params.queueId
-    const targetUrl = 'https://example.com';
+    const team = await fetchTeamInfo(request.params.queueId);
+
     // VRTの実行
-    await mainVrt(targetUrl);
+    await mainVrt(team.pageUrl);
     // 差分チェック
     // TODO: save vrt result to storage
     execSync('reg-cli ./tmp/actual ../vrt/expected ./tmp/diff -M 0.15 -T 0.005 -I -J ./tmp/reg.json');
     // スコアの取得
-    // TODO: save scoring result to database
-    const result = await mainScoring(request.params.queueId, targetUrl);
-
+    const { result } = await mainScoring(request.params.queueId, team.pageUrl);
+    if ('error' in result) {
+      throw result.error;
+    }
+    // スコアの記録
+    await createMeasurement(team.id, result.score);
     await updateQueueStatus(request.params.queueId, 'DONE');
+
     return result;
   } catch (e) {
     await updateQueueStatus(request.params.queueId, 'FAILED');
